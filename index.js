6da7fe8f1b7fb7395565cce636c1ffe04e1ab5c8
Ryan Lebel <ryan@firescript.ca>
2016-01-14 15:25:46 -0500

    push

---
+++ index.js
@@ -0,0 +1,43 @@
+var frameModule = require("ui/frame");
+
+var CustomCNContactPickerViewControllerDelegate = NSObject.extend({    
+    initWithResolveReject: function(resolve, reject) {
+        var self = this.super.init();
+        if(self) {
+            this.resolve = resolve;
+            this.reject = reject;
+        }
+        return self;
+    },
+    contactPickerDidCancel: function(controller){
+        this.resolve({
+           response: "cancelled",
+           ios: null,
+           android: null 
+        });
+    },
+    contactPickerDidSelectContact: function(controller, contact) {        
+        controller.dismissModalViewControllerAnimated(true);
+        this.resolve({
+            response: "selected",
+            ios: contact,
+            android: null
+        });        
+        CFRelease(controller.delegate);
+    }
+}, {
+    protocols: [CNContactPickerDelegate]
+});
+
+function pick(){
+    return new Promise(function (resolve, reject) {  
+        var controller = CNContactPickerViewController.alloc().init();
+        var delegate = CustomCNContactPickerViewControllerDelegate.alloc().initWithResolveReject(resolve, reject);
+        CFRetain(delegate);
+        controller.delegate = delegate;
+        var page = frameModule.topmost().ios.controller;
+        page.presentModalViewControllerAnimated(controller, true);
+    });
+}
+
+exports.pick = pick;
